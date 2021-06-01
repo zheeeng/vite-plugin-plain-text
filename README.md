81d09e1a4d9fa0b15c1fbbb8ba5034737f5691b7
zheeeng <hi@zheeeng.me>
2021-06-01 08:52:28 +0800

    u pkg

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ![example workflow](https://github.com/zheeeng/vite-plugin-plain-text/actions/workflows/publish.yml/badge.svg)
 [![npm version](https://img.shields.io/npm/v/vite-plugin-plain-text.svg)](https://www.npmjs.com/package/vite-plugin-plain-text)
 
-A vite plugin load file as plain text
+A Vite plugin transforms the rule matched file as plain text
 
 ## Install
 
@@ -16,15 +16,31 @@ yarn add -D vite-plugin-plain-text (or by npm)
 ## Usage
 
 ```ts
+// vite.config.ts
+
 import { defineConfig } from 'vite';
 
+/**
+ * @param match Regular expression in string or Regexp type,
+ * or a match predicate  (this: vite transform context, code: string, id: file name string) => void
+ * @returns transformed code
+ */
 import plainText from 'vite-plugin-plain-text';
 
 export default defineConfig({
   plugins: [
+    // passing string type Regular expression
     plainText(/\/LICENSE$/),
   ],
 });
+```
+
+```ts
+// component.ts
+
+import { plainText as LICENSE } from '@root/LICENSE'
+
+console.log(LICENSE)
 ```
 
 ## License
--- index.ts
+++ index.ts
@@ -4,20 +4,25 @@ type ThisParameter<T extends undefined | ((...args: any) => any)> = T extends (t
 
 type TransformContext = ThisParameter<Plugin['transform']>
 
-export function plainText (match: string | RegExp | ((this: TransformContext, code: string, id: string) => void)): Plugin {
-    return {
-      name: 'plain text',
-      transform (code, id) {
-        if (
-          typeof match === 'string' && new RegExp(match).test(id)
-          || match instanceof RegExp && match.test(id)
-          || typeof match === 'function' && match.call(this, code, id)
-        ) {
-          return `export default () => ${JSON.stringify(code)}`
-        }
-  
-        return code
-      },
-    }
+/**
+ * @param match Regular expression in string or Regexp type,
+ * or a match predicate  (this: vite transform context, code: string, id: file name string) => void
+ * @returns transformed code
+ */
+export default function plainText (match: string | RegExp | ((this: TransformContext, code: string, id: string) => boolean)): Plugin {
+  return {
+    name: 'plain text',
+    transform (code, id) {
+      if (
+        typeof match === 'string' && new RegExp(match).test(id)
+        || match instanceof RegExp && match.test(id)
+        || typeof match === 'function' && match.call(this, code, id)
+      ) {
+        return `export const plainText = ${JSON.stringify(code)}`
+      }
+
+      return code
+    },
   }
+}
   
