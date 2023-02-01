2ed3be76a6f561cfff34c09275ef98b412e83e06
Zheeeng <hi@zheeeng.me>
2023-02-01 11:53:03 +0800

    feat: support array input

--- src/index.ts
+++ src/index.ts
@@ -3,23 +3,29 @@ import { MagicString } from '@napi-rs/magic-string'
 
 type TransformContext = ThisParameterType<Plugin['transform']>
 
+type Match = string | string[] | RegExp | RegExp[]
+
+type MatchFn = (this: TransformContext, code: string, id: string) => boolean
+
+const testMatch = (match: Match, testId: string): boolean => false
+  || typeof match === 'string' && new RegExp(match).test(testId)
+  || match instanceof RegExp   && match.test(testId)
+  || Array.isArray(match)      && match.some(m => testMatch(m, testId))
+
 /**
- * @param {string | RegExp | Function} match
+ * @param {string | RegExp | string[] | RegExp[] | Function} match
  *  String or RegExp to match the module id(file name),
  *  it also can be a matching-predicator with the signature `(this: vite transform context, code: string, id: file name string) => void`
  * @return transformed code
  */
-export default function plainText (match: string | RegExp | ((this: TransformContext, code: string, id: string) => boolean)): Plugin {
+export default function plainText (match: Match | MatchFn): Plugin {
   return {
     name: 'plain text',
     transform (code, id) {
-      if (
-        typeof match === 'string' && new RegExp(match).test(id)
-        || match instanceof RegExp && match.test(id)
-        || typeof match === 'function' && match.call(this, code, id)
-      ) {
-
+      if (typeof match === 'function' ? match.call(this, code, id) : testMatch(match, id)) {
         const magicString = new MagicString(code)
+
+        // ~~ effect ~~
         magicString.overwrite(0, code.length, `export const plainText = ${JSON.stringify(code)}`)
 
         const sourcemap = this.getCombinedSourcemap()
